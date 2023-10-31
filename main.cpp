bb47bcbd8f53f1baaff8388d61505876297e4ea9
marraccinom30 <55815428+marraccinom30@users.noreply.github.com>
2023-10-31 16:03:07 -0400

    10.31.23
    lab

--- main.cpp
+++ main.cpp
@@ -9,20 +9,34 @@
 
 using namespace std;
 ifstream OpenInputFile();
-
+void displayVehicles(vector<car> carin) const; 
 int main() {
-    //vector<student> stud_vec;
-    //ifstream playerlist = OpenInputFile(); //opens input file into object
+    vector<student> stud_vec;
+    student tempplayer;
+    //vector<car> car_vec;
+    ifstream playerlist = OpenInputFile(); //opens input file into object
     string stFirstName;
     string stLastName;
-    string stPoints;
+    int stPoints;
+    int driverPoint = -1;
     int selection = -2;
     bool quit = false;
-//    while (playerlist.good()) { // loops until all of the in file has been read
-//        playerlist >> stFirstName >> stLastName >> stPoints;
-//        Song tempplayer(stFirstName, stLastName, stPoints); //take in three arguments then creates a temp object to add to vector
-//        stud_vec.push_back(tempplayer);
-//    }
+    
+
+    //load first 9 (no credits just drivers)
+    for(int i = 0; i < 9; i++){
+        playerlist >> stFirstName >> stLastName;
+        tempplayer.setStudent(stFirstName, stLastName, driverPoint);//Song tempplayer(stFirstName, stLastName, stPoints); //take in three arguments then creates a temp object to add to vector
+        stud_vec.push_back(tempplayer);
+        }
+
+    //load students data into student vector
+   while (playerlist.good()) { // loops until all of the in file has been read
+        playerlist >> stFirstName >> stLastName >> stPoints;
+        tempplayer.setStudent(stFirstName, stLastName, stPoints);
+        stud_vec.push_back(tempplayer);
+   }
+
     while (!(selection == -1)) { //loops until exit command has been given
         selection = -2;
         cout << "Reservation System Menu : " << endl;
@@ -35,33 +49,54 @@ int main() {
         cout << "Select => ";
         cin >> selection;
         cout << endl;
+        
+        string fName;
 
         switch(selection){
-            case 0: //create a reservation
-                //displayVehicles();
-                string fName;
-                cout << "Enter your first name: ";
-                cin >> fName;
-                bool checkName = false;
-                for(int i = 0; i < stud_vec)
-                break;
+            case 0: 
+            //create a reservation
+            //displayVehicles();
+            int tempCredit;
+            cout << "Enter your first name: ";
+            cin >> fName;
+            bool checkName = false;
+            for(int i = 9; i < stud_vec.size(); i++){
+                if(fName = stud_vec[i].firstName){
+                    checkName = true;
+                    stud_vec[i].displayCredits();
+                }
+                else{
+                    cout << "Student name not in menu" << endl;
+                    break;
+                }
 
+            }
+            break;
+            case 3: //display Reservations
+               //displayVehicles(car_vec);
+            break;
         }
 
 
     }
     return 0;
 }
-//ifstream OpenInputFile()
-//{
-//    string inFileName = "";
-//    ifstream temp;
-//
-//    temp.open(inFileName.c_str());
-//
-//    if (!temp.is_open()) { //checks to see if file opened correctly
-//        cout << "\nUnsuccessfully... Program Terminated\n" << endl;
-//        exit(1);
-//    }
-//    return (temp);
-//}
+void displayVehicles(vector<car> carin){
+    cout<<setw(10) << "Truck " << " Compact " << " Sedan " <<endl;
+    cout<<setw(10) << " ------ " << " ------ " << " ------ " <<endl;
+    cout<<setw(10) << " Purple " << " Red " <<" Blue " 
+};
+
+ifstream OpenInputFile()
+{
+   string inFileName = "dodgeball_team.txt";
+   ifstream temp;
+
+   temp.open(inFileName.c_str());
+
+   if (!temp.is_open()) { //checks to see if file opened correctly
+       cout << "\nUnsuccessfully... Program Terminated\n" << endl;
+       exit(1);
+   }
+   return (temp);
+};
